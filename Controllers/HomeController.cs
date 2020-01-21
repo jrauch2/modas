c789f42b2a688b02a82032182a2bd6fdb923bf41
Joshua Rauch <jrauch2@my.wctc.edu>
2020-01-20 19:25:27 -0600

    Add Index View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index() => "This is POCO";
+        public ViewResult Index() => View();
     }
 }
